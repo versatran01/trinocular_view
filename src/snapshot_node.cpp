aee96220f8432f7236d3cd3936cfddb7669b57f5
Chao Qu <quchao@seas.upenn.edu>
2015-11-16 12:19:03 -0500

    commit for renaming

--- src/snapshot_node.cpp
+++ src/snapshot_node.cpp
@@ -1,3 +1,21 @@
 #include <ros/ros.h>
 
-int main(int argc, char** argv) { ros::init(argc, argv, "snapshot_node"); }
+namespace snapshot {
+
+class SnapshotNode {
+ public:
+  SnapshotNode(const ros::NodeHandle& pnh);
+
+ private:
+  ros::NodeHandle pnh_;
+};
+
+SnapshotNode::SnapshotNode(const ros::NodeHandle& pnh) : pnh_(pnh) {
+}
+
+}  // namespace snapshot
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "snapshot_node");
+  ros::NodeHandle nh, pnh("~");
+}
